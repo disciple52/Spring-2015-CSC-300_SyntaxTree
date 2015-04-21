fa6663bda35912846dea3378c2f57ad9d8b6d429
Austin Meier <diveintotheword@att.net>
2015-04-21 15:35:53 -0500

    HW 15 Syntax Tree

--- Driver.java
+++ Driver.java
@@ -2,7 +2,7 @@ public class Driver
 {
 	public static void main(String[] args)
 	{
-		Parser p = new Parser("    a   = (a *  b) - c;");
+		Parser p = new Parser("    a   = (a *   (a+b)) - c;");
 		Parser p2 = new Parser("    a = b - c;");
 		
 		p.parse();
--- Parser.java
+++ Parser.java
@@ -1,7 +1,7 @@
 public class Parser 
 {
 	private String theStmt;
-	private int pos; //where am I in the statement string?
+	private int pos;
 	
 	public Parser(String theStmt)
 	{
@@ -16,24 +16,81 @@ public class Parser
 	
 	private void parse_stmt()
 	{
-		System.out.println("hello");
+		theStmt = theStmt.replaceAll("\\s","");
+		System.out.println(theStmt);
 		//Print each time it reads something like:
-		//Read: VarName = a
-		//Reading: Math-Exprsds
+		// Read VarName = a;
+		String varName = "";
+		for (int i = this.pos; i < theStmt.indexOf("="); i++)
+		{
+			varName = varName + theStmt.charAt(i);
+			pos++;
+		}
+		pos++; 
+		System.out.println("var Name: " + varName);
 		
-		//read a var name
+		// REading: math-expr
+		String mathExpr = theStmt.substring(pos);
+		System.out.println("Reading Math Expr: " + mathExpr);
+		parse_math_expr();		
+		//read var name		
 		//read a math_expr
 	}
 	
 	private void parse_math_expr()
 	{
-		//Display
-		//Reading Left:
-		//Read OP = *
-		//Reading Right:
-		
-		//read left
-		//read op
-		//read right
+		String reading = "";
+		if(theStmt.indexOf("(") == pos)
+		{
+			for(int i = pos; i <= theStmt.indexOf(")"); i++)
+			{
+				reading = reading + theStmt.charAt(i);
+				pos++;
+			}
+			System.out.println("Reading Left: " + reading);
+			parse_math(reading);
+			System.out.println("Read Op: " + theStmt.charAt(pos));
+			pos++;
+			String parseRight = theStmt.substring(pos,theStmt.length()-1);
+			System.out.println("Reading Right: " + parseRight);
+		}
+		else
+		{
+			System.out.println("Reading Left: " + theStmt.charAt(pos));
+			pos++;
+			System.out.println("Op: " + theStmt.charAt(pos));
+			pos++;
+			System.out.println("Reading Right: " + theStmt.substring(pos,theStmt.length()-1));
+			parse_math(theStmt.substring(pos,theStmt.length()-1));
+		}
+	}
+	
+	private void parse_math(String toParse)
+	{
+		int currPos = 0;
+		String toLeft = "";
+		for(int i = 1; i < toParse.indexOf(")"); i++)
+		{
+			toLeft = toLeft + toParse.charAt(i);
+		}
+		if(toLeft.charAt(currPos) != '(')
+		{
+			System.out.println("Reading Left: " + toLeft.charAt(currPos));
+			currPos++;
+			System.out.println("Read Op: " + toLeft.charAt(currPos));
+			currPos++;
+			if(toLeft.charAt(currPos) != '(')
+			{
+				System.out.println("Reading Right: " + toLeft.substring(currPos));
+			}
+			else
+			{
+				parse_math(toLeft.substring(currPos));
+			}
+		}
+		else
+		{
+			parse_math(toLeft.substring(currPos));
+		}		
 	}
 }
